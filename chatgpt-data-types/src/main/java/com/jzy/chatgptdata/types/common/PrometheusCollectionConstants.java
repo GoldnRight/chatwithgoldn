c1001759ec5243419ac2cb7eeef7b426de92c25b
jinzhenyu <1102984100@qq.com>
2025-08-29 21:47:38 +0800

    fix

--- chatgpt-data-types/src/main/java/com/jzy/chatgptdata/types/common/PrometheusCollectionConstants.java
+++ chatgpt-data-types/src/main/java/com/jzy/chatgptdata/types/common/PrometheusCollectionConstants.java
@@ -4,5 +4,4 @@ public class PrometheusCollectionConstants {
     public static final String CHAT_COMPLETIONS_TOTAL = "chat_completions_total";
     public static final String CHAT_COMPLETIONS_DURATION = "chat_completions_duration";
     public static final String CHAT_FIRST_TOKEN_LATENCY = "chat_first_token_latency";
-    public static final String CHAT_COMPLETIONS_USAGE = "chat_completions_usage";
 }
